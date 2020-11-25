10aa5e7c45872bb11b0b2015fa558117b6d4cde7
Albina <albinamuzafari@gmail.com>
2020-11-25 21:38:08 +0100

    update the server side to persist new users using hibernate

---
+++ Quieasy/src/domain/UserC.java
@@ -0,0 +1,64 @@
+// class for user and account related operations such as registration, log in/out
+
+package domain;
+
+import application.ClientAgent;
+import data.Message;
+import data.RegisterData;
+import data.LoginData;
+
+public class UserC {
+	
+	private static UserC userC;
+	
+	// user data
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	// constructor can only be accessed from within
+	private UserC(String firstName, String lastName, String email) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		
+	}
+	
+	// Gets the current instance -> Singleton.
+	public static UserC getUser(String firstName, String lastName, String email) {
+		
+		if (userC == null) userC = new UserC(firstName, lastName, email);
+		
+		return userC;
+		
+	}
+	
+	// create a new account
+	public static void register(String firstName, String lastName, String email, String password) {
+		
+		ClientAgent clientAgent = ClientAgent.getClientAgent();
+		Message message = new Message();
+		message.task = "REGISTER";
+		message.registerData = new RegisterData(firstName, lastName, email, password);
+		clientAgent.send(message);
+		
+	}
+	
+	// log user in
+	public static void login(String email, String password) {
+		
+		ClientAgent clientAgent = ClientAgent.getClientAgent();
+		Message message = new Message();
+		message.task = "LOG_IN";
+		message.loginData = new LoginData(email, password);
+		clientAgent.send(message);
+		
+	}
+	
+	// log user out
+	public static void logout() {
+		userC = null;
+	}
+	
+}
