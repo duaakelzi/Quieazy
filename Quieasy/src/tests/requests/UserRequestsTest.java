bc87af7ac68dfb1a8c8e043d83544813f7f6e084
Albina <albinamuzafari@gmail.com>
2021-01-09 23:15:39 +0100

    added a comment

--- Quieasy/src/tests/requests/UserRequestsTest.java
+++ Quieasy/src/tests/requests/UserRequestsTest.java
@@ -4,7 +4,7 @@ import data.UserData;
 import junit.framework.TestCase;
 
 public class UserRequestsTest extends TestCase {
-
+    //still to be developed
     public void testLogin() {
         UserData nonExistingUser = new UserData("Zefrank", "Zefrankov", "zefrank@funny.com");
         String password = "123";
