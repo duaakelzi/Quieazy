3c5ed629a9c5977ac8354571e8230022772009ba
icicala <icicala.ubb@gmail.com>
2021-01-14 23:47:06 +0100

    Store selection of user in a HashTable, store the selection of user when navigate pagination, change the implementation of radiobuttons.

--- Quieasy/src/gui/PlayQuizBox.java
+++ Quieasy/src/gui/PlayQuizBox.java
@@ -1,10 +1,10 @@
 package gui;
 
 import data.ChoicesData;
-import data.Message;
 import data.QuestionData;
 import data.QuizData;
 import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -20,9 +20,10 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
-import requests.CheckCorrectAnswerC;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayQuizBox extends VBox {
 
@@ -46,6 +47,13 @@ public class PlayQuizBox extends VBox {
     ProgressIndicator indicator;
     Pagination trackquestions;
     private final int NUMBEROFCHOICES = 4;
+    RadioButton choice_1;
+    RadioButton choice_2;
+    RadioButton choice_3;
+    RadioButton choice_4;
+    ToggleGroup groupOfAnswers;
+    VBox answers;
+    Map<String, String> selectedAnswerUser = new HashMap<>();
   //  private ArrayList<ChoicesData> questionChoices;
 
     //constructor*****
@@ -64,7 +72,7 @@ public class PlayQuizBox extends VBox {
         HBox markQuestion = initiateQuestionMarkandQuestion();
 
         // Answer layout
-        VBox answers = initiateAnswers();
+        answers = initiateAnswers();
 
         //Buttons
         HBox buttons = initiateButtons();
@@ -100,7 +108,13 @@ public class PlayQuizBox extends VBox {
          trackquestions.currentPageIndexProperty().addListener(new ChangeListener<Number>() {
              @Override
              public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                 cleanChoicesSelection();
                  fillQuestionChoicesWithdata(observableValue.getValue().intValue());
+//                 if(checkChoicesIfSelected()){
+////                     String selectionUser = selectedAnswerUser.get(quizQuestions.get(observableValue.getValue().intValue()));
+////                     System.out.println("What user have been selected" +selectionUser);
+//
+//                 }
                  System.out.println("Observale " + observableValue.getValue().intValue() + " number: " + number + " t1 :" + t1);
              }
          });
@@ -136,16 +150,12 @@ public class PlayQuizBox extends VBox {
         minutes.setTextFill(Color.BLACK);
         dot.setTextFill(Color.BLACK);
         seconds.setTextFill(Color.BLACK);
-
         tilePaneTImer.getChildren().addAll(minutes,dot, seconds);
         countDownTimerQuiz();
-
-
         //for first view only
         //textMark.setText("Question " + (indexQuestion+1) +  "\n Quiz points: " + quizQuestions.get(indexQuestion).getPoints()); // additional info: status (answered/not), points (out of total)
         mark.getChildren().addAll(textMark, tilePaneTImer);
         mark.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-
         HBox question = new HBox();
         question.setBackground(new Background(new BackgroundFill(Color.LIGHTSKYBLUE, null, null)));
         question.setPrefWidth(535);
@@ -164,29 +174,40 @@ public class PlayQuizBox extends VBox {
    // shows the choices for the question
     public VBox initiateAnswers(){
         VBox answers = new VBox(20);
-        ToggleGroup group = new ToggleGroup();
+        groupOfAnswers = new ToggleGroup();
         answers.setPrefWidth(650);
-        answersCheck = new RadioButton[4];
-        selectedAnswer =new String[quiz.getQuestions().size()];
+        //answersCheck = new RadioButton[NUMBEROFCHOICES];
+       // selectedAnswer =new String[quiz.getQuestions().size()];
         answers.setBackground(new Background(new BackgroundFill(Color.LIGHTCYAN, null, null)));
         answers.setPadding(new Insets(20, 10, 10, 140));
-        for(int i= 0; i < NUMBEROFCHOICES; i++){
+       // for(int i= 0; i < NUMBEROFCHOICES; i++){
             //for first view only
-            RadioButton answer = new RadioButton();
-            answersCheck[i]=answer;
-            answer.setFont(Font.font("Times New Roman", 16));
+           // RadioButton answer = new RadioButton();
+            //answersCheck[i]=answer;
+           // answer.setFont(Font.font("Times New Roman", 16));
 //            System.out.println(answer.getText());
 //            System.out.println(quizQuestions.get(indexQuestion).getAnswers().get(i).getChoiceDescription()); //test
-            answersCheck[i].setToggleGroup(group);
-            answers.getChildren().addAll(answersCheck[i]);
-            answer.selectedProperty().addListener((observableValue, old_value, new_val) -> {
-               boolean selected =answer.isSelected();
-               if (selected){System.out.println("selected :"+answer.getText());
-               selectedAnswer[indexQuestion]=answer.getText();;
-               }
-               else {{System.out.println("not selected :"+answer.getText());}}//what should happen here?
-            });
-        }
+            choice_1 = new RadioButton();
+            choice_2 = new RadioButton();
+            choice_3 = new RadioButton();
+            choice_4 = new RadioButton();
+            choice_1.setToggleGroup(groupOfAnswers);
+            choice_2.setToggleGroup(groupOfAnswers);
+            choice_3.setToggleGroup(groupOfAnswers);
+            choice_4.setToggleGroup(groupOfAnswers);
+            choice_1.setFont(Font.font("Times New Roman", 16));
+            choice_2.setFont(Font.font("Times New Roman", 16));
+            choice_3.setFont(Font.font("Times New Roman", 16));
+            choice_4.setFont(Font.font("Times New Roman", 16));
+            answers.getChildren().addAll(choice_1, choice_2, choice_3, choice_4);
+//            answer.selectedProperty().addListener((observableValue, old_value, new_val) -> {
+//               boolean selected =answer.isSelected();
+//               if (selected){System.out.println("selected :"+answer.getText());
+//               selectedAnswer[indexQuestion]=answer.getText();;
+//               }
+//               else {{System.out.println("not selected :"+answer.getText());}}//what should happen here?
+//            });
+      //  }
         return answers;
     }
 
@@ -220,8 +241,19 @@ public class PlayQuizBox extends VBox {
         next.setEffect(new DropShadow());
         //so far doesn't work. same behavior expected of the questionsTrack
         next.setOnAction(actionEvent -> {
-                    indexQuestion++;
-                    fillQuestionChoicesWithdata(indexQuestion);
+                    if(checkChoicesIfSelected()) {
+                        indexQuestion++;
+                        if(indexQuestion != quizQuestions.size()){cleanChoicesSelection();}
+                        fillQuestionChoicesWithdata(indexQuestion);
+
+
+                    }else{
+
+                        answers.setBorder(new Border(new BorderStroke(Color.FIREBRICK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+                        PauseTransition pause = new PauseTransition(Duration.seconds(3));
+                        pause.setOnFinished(actionEvent1 -> answers.setBorder(null));
+                        pause.play();
+                    }
 
 
 
@@ -231,6 +263,7 @@ public class PlayQuizBox extends VBox {
 //                initiateAnswers(i);
 //            }
         });
+
         return next;
     }
     // method to finish quiz, save user input, check for correctness, send results to the server and display to user
@@ -242,16 +275,17 @@ public class PlayQuizBox extends VBox {
         ImageView submitImg = new ImageView(new Image("images/submit.png"));
         submit.setGraphic(submitImg);
         submit.setOnAction(e->{
+            selectedAnswerUser.forEach((key, value) -> System.out.println(key + " the answer from " + value));
             //on button submit action
             // here local checking of correctly answered questions can take place, since client already has the data
             // the number of correctly answered questions will be sent to server, along with info on quiz&user
             // concurrently, user can be informed about the results of the played quiz
             // options to repeat or go back to QuizBrowser should be offered
             // !! at least one question should be answered!!
-            CheckCorrectAnswerC ch=new CheckCorrectAnswerC();
-            System.out.println(selectedAnswer[0]);
-            Message i=ch.checkAnswers(quiz,selectedAnswer);
-            System.out.println("play Result"+i);
+//            CheckCorrectAnswerC ch=new CheckCorrectAnswerC();
+//            //System.out.println(selectedAnswer[0]);
+//            Message i=ch.checkAnswers(quiz,selectedAnswer);
+           // System.out.println("play Result"+i);
             MainPane.getMainPane().getTabs().add(QuizFinalResultTab.getQuizFinalResultTab());
            // MainPane.getMainPane().getTabs().add(new Tab("result",new CreateQuizResultBox(i)));
             CreateAddQuestionTab.getCreateAddQuestionTab().closeTab();
@@ -269,46 +303,96 @@ public class PlayQuizBox extends VBox {
         return cancel;
     }
 
-    private void initiateDataPlayQuiz(int pos){
-
-            this.textMark.setText("Question " + (pos+1) + "\nPoints: " + quizQuestions.get(pos).getPoints());
-            this.questionText.setText(quizQuestions.get(pos).getQuestion());
-
-    }
-
     private void fillQuestionChoicesWithdata(int indexofQuestion){
-        try {
+            try {
 
-            double progres = (indexQuestion*1.0)/quizQuestions.size();
-            indicator.setProgress(progres);
-            if (indexQuestion==quizQuestions.size()){indexQuestion=0;}
-            else{
+                double progres = (indexQuestion * 1.0) / quizQuestions.size();
+                indicator.setProgress(progres);
+                if (indexQuestion == quizQuestions.size()) {
+                    indexQuestion = 0;
+                } else {
 
-                this.textMark.setText("Question " + (indexofQuestion+1) + "\nPoint : " + quizQuestions.get(indexofQuestion).getPoints());
-                trackquestions.setCurrentPageIndex(indexofQuestion);
-                this.questionText.setText(quizQuestions.get(indexofQuestion).getQuestion());
-                for (int i = 0; i < NUMBEROFCHOICES; i++) {
-                    answersCheck[i].setText(quizQuestions.get(indexofQuestion).getAnswers().get(i).getChoiceDescription());
-                    answersCheck[i].setSelected(false);
-                    RadioButton choicesOfQuestionsRadioButtons=answersCheck[i];
-                    choicesOfQuestionsRadioButtons.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                    this.textMark.setText("Question " + (indexofQuestion + 1) + "\nPoint : " + quizQuestions.get(indexofQuestion).getPoints());
+                    trackquestions.setCurrentPageIndex(indexofQuestion);
+                    this.questionText.setText(quizQuestions.get(indexofQuestion).getQuestion());
+                   try {
+                       choice_1.setText(quizQuestions.get(indexofQuestion).getAnswers().get(0).getChoiceDescription());
+
+                       choice_2.setText(quizQuestions.get(indexofQuestion).getAnswers().get(1).getChoiceDescription());
+
+                       choice_3.setText(quizQuestions.get(indexofQuestion).getAnswers().get(2).getChoiceDescription());
+
+                       choice_4.setText(quizQuestions.get(indexofQuestion).getAnswers().get(3).getChoiceDescription());
+
+                       if(indexofQuestion < selectedAnswerUser.size()) {
+                           if (choice_1.getText().equals(selectedAnswerUser.get(quizQuestions.get(indexofQuestion).getQuestion()))) {
+                               System.out.println("Dataselectedby user " + selectedAnswerUser.get(quizQuestions.get(indexofQuestion).getQuestion()));
+                               choice_1.setSelected(true);
+                           }
+                           if (choice_2.getText().equals(selectedAnswerUser.get(quizQuestions.get(indexofQuestion).getQuestion()))){
+                               choice_2.setSelected(true);
+                           }
+                           if (choice_3.getText().equals(selectedAnswerUser.get(quizQuestions.get(indexofQuestion).getQuestion()))){
+                               choice_3.setSelected(true);
+                           }
+                           if (choice_4.getText().equals(selectedAnswerUser.get(quizQuestions.get(indexofQuestion).getQuestion()))){
+                               choice_4.setSelected(true);
+                           }
+                       }
+                   }catch (IndexOutOfBoundsException e){
+                       System.out.println("Index out of boundary");
+                   }
+
+
+                    groupOfAnswers.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
                         @Override
-                        public void changed(ObservableValue<? extends Boolean> observableValue, Boolean old_value, Boolean new_val) {
-                            boolean selected =choicesOfQuestionsRadioButtons.isSelected();
-                            if (selected){System.out.println("selected :"+choicesOfQuestionsRadioButtons.getText());
-                                selectedAnswer[indexQuestion]=choicesOfQuestionsRadioButtons.getText();;
+                        public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                            //  selectedAnswerUser.put(quizQuestions.get(trackquestions.getCurrentPageIndex()), t1.getUserData().toString() );
+                            if (checkChoicesIfSelected()) {
+
+                                selectedAnswerUser.put(questionText.getText(),((RadioButton)observableValue.getValue()).getText());
+
                             }
-                            else {{System.out.println("not selected :"+choicesOfQuestionsRadioButtons.getText());}}//what should happen here?
+
                         }
                     });
+
+//                for (int i = 0; i < NUMBEROFCHOICES; i++) {
+//                    answersCheck[i].setText(quizQuestions.get(indexofQuestion).getAnswers().get(i).getChoiceDescription());
+//                    answersCheck[i].setSelected(false);
+//                    RadioButton choicesOfQuestionsRadioButtons=answersCheck[i];
+//                    choicesOfQuestionsRadioButtons.selectedProperty().addListener(new ChangeListener<Boolean>() {
+//                        @Override
+//                        public void changed(ObservableValue<? extends Boolean> observableValue, Boolean old_value, Boolean new_val) {
+//                            boolean selected =choicesOfQuestionsRadioButtons.isSelected();
+//                            if (selected){System.out.println("selected :"+choicesOfQuestionsRadioButtons.getText());
+//                                selectedAnswer[indexQuestion]=choicesOfQuestionsRadioButtons.getText();;
+//                            }
+//                            else {{System.out.println("not selected :"+choicesOfQuestionsRadioButtons.getText());}}//what should happen here?
+//                        }
+//                    });
+//                }
+
                 }
+
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(" I am out of boundary");
 
             }
 
-        }catch (IndexOutOfBoundsException e){
-            System.out.println(" I am out of boundary");
 
-        }
+
+    }
+
+    private void cleanChoicesSelection(){
+        choice_1.setSelected(false);
+        choice_2.setSelected(false);
+        choice_3.setSelected(false);
+        choice_4.setSelected(false);
+    }
+
+    private boolean checkChoicesIfSelected() {
+        return groupOfAnswers.getSelectedToggle() != null;
     }
 
 
